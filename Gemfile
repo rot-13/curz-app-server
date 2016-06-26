3802107e721ca6ebec63f0099cbb61023aa8a933
Assaf Gelber <assaf.gelber@gmail.com>
2016-06-26 18:47:51 +0300

    Drop espeak, pass on text

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ ruby '2.3.0'
 gem 'rake'
 gem 'puma'
 gem 'sinatra'
-gem "espeak-ruby"
 
 group :development do
   gem 'tux'
--- handlers.rb
+++ handlers.rb
@@ -1,19 +1,12 @@
 require 'net/http'
-require 'espeak'
 
-include ESpeak
-
-STREAM_URI = URI('https://cpc-curz.herokuapp.com/play_file')
+STREAM_FILE_URI = URI('https://cpc-curz.herokuapp.com/play_file')
+STREAM_TEXT_URI = URI('https://cpc-curz.herokuapp.com/play_text')
 
 def handle_file(file_object)
-  Net::HTTP.post_form(STREAM_URI, 'file' => file_object)
+  Net::HTTP.post_form(STREAM_FILE_URI, 'file' => file_object)
 end
 
 def handle_text(text)
-  file_path = './public/text-#{Time.now.getutc}.mp3'
-  speech = Speech.new(text)
-  speech.save(file_path)
-  File.open(file_path, 'r') do |file|
-    Net::HTTP.post_form(STREAM_URI, 'file' => file)
-  end
+  Net::HTTP.post_form(STREAM_TEXT_URI, 'text' => text)
 end
