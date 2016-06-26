2cddbe7dc236806510422d9ad267f2c037e903fa
Assaf Gelber <assaf.gelber@gmail.com>
2016-06-26 19:48:01 +0300

    Remove espeak

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ ruby '2.3.0'
 gem 'rake'
 gem 'puma'
 gem 'sinatra'
-gem 'espeak-ruby'
 gem 'multipart-post'
 
 group :development do
--- handlers.rb
+++ handlers.rb
@@ -1,11 +1,9 @@
 require 'net/http'
 require 'net/http/post/multipart'
-require 'espeak'
-
-include ESpeak
 
 STREAM_FILE_URI = URI('https://cpc-curz.herokuapp.com/play_file')
 STREAM_URL_URI  = URI('https://cpc-curz.herokuapp.com/play_url')
+STREAM_TEXT_URI = URI('https://cpc-curz.herokuapp.com/play_text')
 
 def handle_file(file_object)
   file = file_object[:tempfile]
@@ -18,11 +16,5 @@ def handle_url(url)
 end
 
 def handle_text(text)
-  file_path = File.join(File.dirname(__FILE__), 'public', "text-#{Time.now.to_i}.mp3")
-  speech = Speech.new(text)
-  speech.save(file_path)
-  File.open(file_path, 'r') do |file|
-    req = Net::HTTP::Post::Multipart.new(STREAM_FILE_URI.path, 'file' => UploadIO.new(file, 'audio/mp3'))
-    http.request(req)
-  end
+  Net::HTTP.post_form(STREAM_TEXT_URI, 'text' => text)
 end
