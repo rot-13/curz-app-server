209e91e69b4c25b602459e9bda019596e9684f74
Assaf Gelber <assaf.gelber@gmail.com>
2016-06-26 18:01:49 +0300

    Add tts

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.3.0'
 gem 'rake'
 gem 'puma'
 gem 'sinatra'
+gem "espeak-ruby"
 
 group :development do
   gem 'tux'
--- handlers.rb
+++ handlers.rb
@@ -1,11 +1,19 @@
 require 'net/http'
+require 'espeak'
 
-STREAM_URL = URI('https://cpc-curz.herokuapp.com/play_file')
+include ESpeak
+
+STREAM_URI = URI('https://cpc-curz.herokuapp.com/play_file')
 
 def handle_file(file_object)
-  Net::HTTP.post_form(STREAM_URL, 'file' => file_object['tempfile'])
+  Net::HTTP.post_form(STREAM_URI, 'file' => file_object)
 end
 
 def handle_text(text)
-
+  file_path = './public/text-#{Time.now.getutc}.mp3'
+  speech = Speech.new(text)
+  speech.save(file_path)
+  File.open(file_path, 'r') do |file|
+    Net::HTTP.post_form(STREAM_URI, 'file' => file)
+  end
 end
