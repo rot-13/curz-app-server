75eb8099cdc5422807fa1bdae95b52814b662e2b
Assaf Gelber <assaf.gelber@gmail.com>
2016-06-26 19:54:50 +0300

    Better mimetype guess

--- handlers.rb
+++ handlers.rb
@@ -7,7 +7,8 @@ STREAM_TEXT_URI = URI('https://cpc-curz.herokuapp.com/play_text')
 
 def handle_file(file_object)
   file = file_object[:tempfile]
-  req = Net::HTTP::Post::Multipart.new(STREAM_FILE_URI.path, 'file' => UploadIO.new(file, 'audio/mp3'))
+  ext = file_object[:file_name].split('.').last
+  req = Net::HTTP::Post::Multipart.new(STREAM_FILE_URI.path, 'file' => UploadIO.new(file, "audio/#{ext}"))
   res = Net::HTTP.start(STREAM_FILE_URI.host, STREAM_FILE_URI.port) do |http|
     http.request(req)
   end
