69551771eb492a4b74fefcbbb987d279840e5c06
Assaf Gelber <assaf.gelber@gmail.com>
2016-06-26 17:46:51 +0300

    Fix uri

--- handlers.rb
+++ handlers.rb
@@ -1,6 +1,6 @@
 require 'net/http'
 
-STREAM_URL = URI('https://cpc-curz.herokuapp.com')
+STREAM_URL = URI('https://cpc-curz.herokuapp.com/play_file')
 
 def handle_file(file_object)
   Net::HTTP.post_form(STREAM_URL, file: file_object['tempfile'])
